7063ce500b679ca73e14d31c6d9b7d723b8d4690
Àngel Mariages <angel.mariages@gmail.com>
2017-02-06 17:46:57 +0100

    Doing changes

--- app/src/main/java/org/angelmariages/rodalieswidget/WidgetManager.java
+++ app/src/main/java/org/angelmariages/rodalieswidget/WidgetManager.java
@@ -4,159 +4,152 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.widget.Toast;
 
-import org.angelmariages.rodalieswidget.timetables.GetTimeTablesRenfe;
+import org.angelmariages.rodalieswidget.timetables.TrainTime;
 import org.angelmariages.rodalieswidget.utils.StationUtils;
 import org.angelmariages.rodalieswidget.utils.U;
 
+import java.util.ArrayList;
+
 public class WidgetManager extends AppWidgetProvider {
 
-    @Override
-    public void onEnabled(Context context) {
-        super.onEnabled(context);
-        U.log("onEnabled");
-    }
+	@Override
+	public void onEnabled(Context context) {
+		super.onEnabled(context);
+		U.log("onEnabled");
+	}
 
-    @Override
-    public void onDeleted(Context context, int[] appWidgetIds) {
-        super.onDeleted(context, appWidgetIds);
-        // TODO: 07-May-16 Borrar shared preferences
-        U.log("onDeleted");
-    }
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		super.onDeleted(context, appWidgetIds);
+		// TODO: 07-May-16 Borrar shared preferences
+		U.log("onDeleted");
+	}
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        U.log("onUpdate; num of widgets:" + appWidgetIds.length);
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		U.log("onUpdate; num of widgets:" + appWidgetIds.length);
 
-        for (int i = 0; i < appWidgetIds.length; i++) {
-            int widgetID = appWidgetIds[i];
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			int widgetID = appWidgetIds[i];
 
-            RodaliesWidget widget = new RodaliesWidget(context, widgetID, U.WIDGET_STATE_UPDATE_TABLES, R.layout.widget_layout);
+			appWidgetManager.updateAppWidget(widgetID, reloadWidget(context, widgetID));
 
-            appWidgetManager.updateAppWidget(widgetID, widget);
+			U.logUpdates(context, widgetID);
 
-            U.logUpdates(context, widgetID);
+			U.log("Updating widget id:" + i + " widgetID? " + widgetID);
+		}
+	}
 
-            U.log("Updating widget id:" + i + " widgetID? " + widgetID);
-        }
-    }
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		String intentAction = intent.getAction();
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-        String intentAction = intent.getAction();
+		U.log("onReceive(); intentAction: " + intentAction);
 
-        U.log("onReceive(); intentAction: " + intentAction);
+		if (intentAction.isEmpty()) return;
 
-        if(intentAction.isEmpty()) return;
+		if (intentAction.startsWith(U.ACTION_SEND_SCHEDULE)) {
+			int widgetID = U.getIdFromIntent(intent);
 
-	    if(intentAction.startsWith(U.ACTION_SEND_SCHEDULE)) {
-		    int widgetID = U.getIdFromIntent(intent);
+			if (intent.hasExtra(U.EXTRA_SCHEDULE_BUNDLE)) {
+				Bundle bundle = intent.getBundleExtra(U.EXTRA_SCHEDULE_BUNDLE);
+				ArrayList<TrainTime> schedule = (ArrayList<TrainTime>) bundle.getSerializable(U.EXTRA_SCHEDULE_DATA);
 
+				loadSchedule(context, widgetID, schedule);
+			}
 
+		} else if (intentAction.startsWith(U.ACTION_CLICK_UPDATE_BUTTON)) {
+			int widgetID = U.getIdFromIntent(intent);
 
-	    } else if(intentAction.startsWith(U.ACTION_CLICK_UPDATE_BUTTON)) {
-            int widgetID = U.getIdFromIntent(intent);
-            int widgetState = U.getStateFromIntent(intent);
+			reloadWidget(context, widgetID);
 
-            updateTimeTables(context, widgetID, widgetState);
+			U.logUpdates(context, widgetID);
+		} else if (intentAction.startsWith(U.ACTION_CLICK_SWAP_BUTTON)) {
+			int widgetID = U.getIdFromIntent(intent);
+			int widgetState = U.getStateFromIntent(intent);
 
-            U.logUpdates(context, widgetID);
-        } else if(intentAction.startsWith(U.ACTION_CLICK_SWAP_BUTTON)) {
-            int widgetID = U.getIdFromIntent(intent);
-            int widgetState = U.getStateFromIntent(intent);
+			swapStations(context, widgetID, widgetState);
+		} else if (intentAction.startsWith(U.ACTION_CLICK_STATIONS_TEXT)) {
+			int widgetID = U.getIdFromIntent(intent);
 
-            swapStations(context, widgetID, widgetState);
-        } else if(intentAction.startsWith(U.ACTION_CLICK_STATIONS_TEXT)) {
-            int widgetID = U.getIdFromIntent(intent);
+			int originOrDestination = intent.getIntExtra(U.EXTRA_OREGNorDESTINATION, -1);
+			if (widgetID != -1 && originOrDestination != -1) {
+				Intent dialogActivity = new Intent(context, SelectStation.class);
+				dialogActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				dialogActivity.putExtra(U.EXTRA_OREGNorDESTINATION, originOrDestination);
+				dialogActivity.putExtra(U.EXTRA_WIDGET_ID, widgetID);
 
-            int originOrDestination = intent.getIntExtra(U.EXTRA_OREGNorDESTINATION, -1);
-            if(widgetID != -1 && originOrDestination != -1) {
-                Intent dialogActivity = new Intent(context, SelectStation.class);
-                dialogActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                dialogActivity.putExtra(U.EXTRA_OREGNorDESTINATION, originOrDestination);
-                dialogActivity.putExtra(U.EXTRA_WIDGET_ID, widgetID);
+				context.startActivity(dialogActivity);
+			}
+		} else if (intentAction.startsWith(U.ACTION_UPDATE_STATIONS)) {
+			int widgetID = U.getIdFromIntent(intent);
 
-                context.startActivity(dialogActivity);
-            }
-        } else if(intentAction.startsWith(U.ACTION_UPDATE_STATIONS)) {
-            int widgetID = U.getIdFromIntent(intent);
+			int newOrigin = intent.getIntExtra(U.EXTRA_ORIGIN, -1);
+			int newDestination = intent.getIntExtra(U.EXTRA_DESTINATION, -1);
 
-            int newOrigin = intent.getIntExtra(U.EXTRA_ORIGIN, -1);
-            int newDestination = intent.getIntExtra(U.EXTRA_DESTINATION, -1);
+			U.log("Got update to: " + widgetID);
+			U.log("UpdateContains: " + newOrigin + "," + newDestination);
 
-            U.log("Got update to: " + widgetID);
-            U.log("UpdateContains: " + newOrigin + "," + newDestination);
+			updateStationTexts(StationUtils.getNameFromID(newOrigin), StationUtils.getNameFromID(newDestination),
+					context, widgetID);
+		} else if (intentAction.startsWith(U.ACTION_CLICK_LIST_ITEM)) {
+			String duracioTrajecte = intent.getStringExtra(U.EXTRA_RIDE_LENGTH);
 
-            updateStationTexts(StationUtils.getNameFromID(newOrigin), StationUtils.getNameFromID(newDestination),
-                    context, widgetID);
-        } else if(intentAction.startsWith(U.ACTION_CLICK_LIST_ITEM)) {
-            String duracioTrajecte = intent.getStringExtra(U.EXTRA_RIDE_LENGTH);
+			Toast.makeText(context, "Duració del trajecte: " + duracioTrajecte, Toast.LENGTH_SHORT).show();
+		} else if (intentAction.startsWith(U.ACTION_WIDGET_NO_DATA)) {
+			int widgetID = U.getIdFromIntent(intent);
+			int widgetState = U.getStateFromIntent(intent);
+			int widgetLayout = R.layout.widget_layout;
 
-            Toast.makeText(context, "Duració del trajecte: " + duracioTrajecte, Toast.LENGTH_SHORT).show();
-        } else if(intentAction.startsWith(U.ACTION_WIDGET_NO_DATA)) {
-            int widgetID = U.getIdFromIntent(intent);
-            int widgetState = U.getStateFromIntent(intent);
-            int widgetLayout = R.layout.widget_layout;
+			if (widgetState == 1 || widgetState == 2 || widgetState == 3)
+				widgetLayout = R.layout.widget_layout_no_data;
 
-            if(widgetState == 1 || widgetState == 2 || widgetState == 3) widgetLayout = R.layout.widget_layout_no_data;
+			AppWidgetManager.getInstance(context).updateAppWidget(widgetID,
+					new RodaliesWidget(context, widgetID, widgetState, widgetLayout, null));
+		}
+	}
 
-            AppWidgetManager.getInstance(context).updateAppWidget(widgetID,
-                    new RodaliesWidget(context, widgetID, widgetState, widgetLayout));
-        }
-    }
+	@Override
+	public void onDisabled(Context context) {
+		super.onDisabled(context);
+		U.log("onDisabled()");
+	}
 
-    @Override
-    public void onDisabled(Context context) {
-        super.onDisabled(context);
-        U.log("onDisabled()");
-    }
+	@Override
+	public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+		super.onRestored(context, oldWidgetIds, newWidgetIds);
+		U.log("onRestored()");
+	}
 
-    @Override
-    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
-        super.onRestored(context, oldWidgetIds, newWidgetIds);
-        U.log("onRestored()");
-    }
+	private void updateStationTexts(String originText, String destinationText, Context context, int widgetID) {
+		if (widgetID != -1) {
+			U.saveStations(context, widgetID, originText, destinationText);
+			reloadWidget(context, widgetID);
+		} else {
+			U.log("ERROR: Widget id not found");
+		}
+	}
 
-    private void updateStationTexts(String originText, String destinationText, Context context, int widgetID) {
-        if(widgetID != -1) {
-            U.saveStations(context, widgetID, originText, destinationText);
-            reloadWidget(context, widgetID);
-        } else {
-            U.log("ERROR: Widget id not found");
-        }
-    }
+	private void swapStations(Context context, int widgetID, int widgetState) {
+		Intent swapIntent = new Intent(context, WidgetReceiver.class);
+		swapIntent.setAction(U.ACTION_CLICK_SWAP_BUTTON);
+		swapIntent.putExtra(U.EXTRA_WIDGET_ID, widgetID);
+		swapIntent.putExtra(U.EXTRA_WIDGET_STATE, widgetState);
+		context.sendBroadcast(swapIntent);
+	}
 
-    private void swapStations(Context context, int widgetID, int widgetState) {
-        Intent swapIntent = new Intent(context, WidgetReceiver.class);
-        swapIntent.setAction(U.ACTION_CLICK_SWAP_BUTTON);
-        swapIntent.putExtra(U.EXTRA_WIDGET_ID, widgetID);
-        swapIntent.putExtra(U.EXTRA_WIDGET_STATE, widgetState);
-        context.sendBroadcast(swapIntent);
-    }
+	private void loadSchedule(Context context, int widgetID, ArrayList<TrainTime> schedule) {
+		RodaliesWidget widget = new RodaliesWidget(context, widgetID, U.WIDGET_STATE_SCHEDULE_LOADED, R.layout.widget_layout, schedule);
+		AppWidgetManager.getInstance(context).updateAppWidget(widgetID, widget);
+	}
 
-    private void updateTimeTables(Context context, int widgetID, int widgetState) {
-        U.log("UpdateTimeTables" + widgetState);
-
-	    int[] stations = U.getStations(context, widgetID);
-	    if(stations.length != 2) {
-		    U.log("Wrong stations! : " + stations.length);
-	    }
-
-	    new GetTimeTablesRenfe(context).execute(stations[0], stations[1], widgetID);
-
-	    if(widgetState != U.WIDGET_STATE_NO_STATIONS) {
-		    RodaliesWidget widget = new RodaliesWidget(context, widgetID, U.WIDGET_STATE_UPDATE_TABLES, R.layout.widget_layout_updating);
-		    AppWidgetManager.getInstance(context).updateAppWidget(widgetID, widget);
-	    } else {
-		    reloadWidget(context, widgetID);
-	    }
-    }
-
-    private void reloadWidget(Context context, int widgetID) {
-        RodaliesWidget widget = new RodaliesWidget(context, widgetID, U.WIDGET_STATE_UPDATE_TABLES, R.layout.widget_layout);
-        AppWidgetManager.getInstance(context).updateAppWidget(widgetID, widget);
-        AppWidgetManager.getInstance(context).notifyAppWidgetViewDataChanged(widgetID, R.id.horarisListView);
-    }
+	private RodaliesWidget reloadWidget(Context context, int widgetID) {
+		RodaliesWidget widget = new RodaliesWidget(context, widgetID, U.WIDGET_STATE_UPDATING_TABLES, R.layout.widget_layout_updating, null);
+		AppWidgetManager.getInstance(context).updateAppWidget(widgetID, widget);
+		return widget;
+	}
 }
--- app/src/main/java/org/angelmariages/rodalieswidget/WidgetService.java
+++ app/src/main/java/org/angelmariages/rodalieswidget/WidgetService.java
@@ -3,26 +3,12 @@ package org.angelmariages.rodalieswidget;
 import android.content.Intent;
 import android.widget.RemoteViewsService;
 
-import org.angelmariages.rodalieswidget.timetables.TrainTime;
 import org.angelmariages.rodalieswidget.utils.U;
-
-import java.io.Serializable;
-import java.util.ArrayList;
 
 public class WidgetService extends RemoteViewsService {
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
         U.log("onGetViewFactory()");
-	    if (intent.hasExtra(U.EXTRA_SCHEDULE_BUNDLE)) {
-		    Serializable serializable = intent.getBundleExtra(U.EXTRA_SCHEDULE_BUNDLE).getSerializable(U.EXTRA_SCHEDULE_DATA);
-		    if(serializable instanceof ArrayList) {
-				ArrayList<TrainTime> scheduleTimes = (ArrayList<TrainTime>) serializable;
-			    for (TrainTime scheduleTime : scheduleTimes) {
-				    System.out.println(scheduleTime.getArrival_time());
-			    }
-			    return new RemoteListViewFactory(this.getApplicationContext(), intent);
-		    }
-	    }
-	    return null;
+	    return new RemoteListViewFactory(this.getApplicationContext(), intent);
     }
 }
