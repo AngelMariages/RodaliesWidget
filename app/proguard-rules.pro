1ecbfc03c742eeeee8112df3bd86c9a525459925
Àngel Mariages <angel.mariages@gmail.com>
2017-03-06 20:58:07 +0100

    Doing changes

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,12 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+-keep class com.firebase.** { *; }
+-keep class org.apache.** { *; }
+-keepnames class com.fasterxml.jackson.** { *; }
+-keepnames class javax.servlet.** { *; }
+-keepnames class org.ietf.jgss.** { *; }
+-dontwarn org.w3c.dom.**
+-dontwarn org.joda.time.**
+-dontwarn org.shaded.apache.**
+-dontwarn org.ietf.jgss.**
---
+++ app/src/main/java/org/angelmariages/rodalieswidget/SettingsActivity.java
@@ -0,0 +1,22 @@
+package org.angelmariages.rodalieswidget;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
+    }
+
+    public static class PreferencesFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_widget);
+        }
+    }
+}
