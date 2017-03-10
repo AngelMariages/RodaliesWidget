55feced0213ace9d933e4a47d0dd7342c97af743
Angel Mariages <angel.mariages@gmail.com>
2017-03-10 20:07:31 +0100

    Polite version 1.6

--- app/src/main/java/org/angelmariages/rodalieswidget/SettingsActivity.java
+++ app/src/main/java/org/angelmariages/rodalieswidget/SettingsActivity.java
@@ -1,9 +1,13 @@
 package org.angelmariages.rodalieswidget;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class SettingsActivity extends AppCompatActivity {
     @Override
@@ -13,10 +17,72 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class PreferencesFragment extends PreferenceFragment {
-        @Override
+	    private FirebaseAnalytics mFirebaseAnalytics;
+
+	    @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+	        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this.getActivity());
             addPreferencesFromResource(R.xml.pref_widget);
+
+	        SwitchPreference show_all_times = (SwitchPreference) findPreference("show_all_times");
+	        SwitchPreference scroll_to_time = (SwitchPreference) findPreference("scroll_to_time");
+	        SwitchPreference download_return_schedule = (SwitchPreference) findPreference("download_return_schedule");
+	        SwitchPreference show_more_transfer_trains = (SwitchPreference) findPreference("show_more_transfer_trains");
+	        SwitchPreference group_transfer_exits = (SwitchPreference) findPreference("group_transfer_exits");
+	        Preference pref_donation = findPreference("pref_donation");
+
+	        show_all_times.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+		        @Override
+		        public boolean onPreferenceChange(Preference preference, Object newValue) {
+			        onPreferenceChangeC("show_all_times", newValue);
+			        return true;
+		        }
+	        });
+
+	        scroll_to_time.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+		        @Override
+		        public boolean onPreferenceChange(Preference preference, Object newValue) {
+			        onPreferenceChangeC("scroll_to_time", newValue);
+			        return true;
+		        }
+	        });
+
+	        download_return_schedule.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+		        @Override
+		        public boolean onPreferenceChange(Preference preference, Object newValue) {
+			        onPreferenceChangeC("download_return_schedule", newValue);
+			        return true;
+		        }
+	        });
+
+	        show_more_transfer_trains.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+		        @Override
+		        public boolean onPreferenceChange(Preference preference, Object newValue) {
+			        onPreferenceChangeC("show_more_transfer_trains", newValue);
+			        return true;
+		        }
+	        });
+
+	        group_transfer_exits.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+		        @Override
+		        public boolean onPreferenceChange(Preference preference, Object newValue) {
+			        onPreferenceChangeC("group_transfer_exits", newValue);
+			        return true;
+		        }
+	        });
+
+	        pref_donation.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		        @Override
+		        public boolean onPreferenceClick(Preference preference) {
+			        onPreferenceChangeC("pref_donation", true);
+			        return false;
+		        }
+	        });
+        }
+
+        private void onPreferenceChangeC(String key, Object newValue) {
+	        mFirebaseAnalytics.setUserProperty(key, newValue.toString());
         }
     }
 }
