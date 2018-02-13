21f416ffbd6f7f3726f41cd53cf68ac364bc63ee
Àngel Mariages <angel.mariages@gmail.com>
2018-02-14 00:23:27 +0100

    quick fix

--- app/src/main/java/org/angelmariages/rodalieswidget/utils/StationUtils.java
+++ app/src/main/java/org/angelmariages/rodalieswidget/utils/StationUtils.java
@@ -29,108 +29,6 @@ import android.graphics.Color;
 import java.util.LinkedHashMap;
 
 public final class StationUtils {
-	/*public static final String[] STATION_NAMES = new String[]{"Aeroport"", "Arenys de Mar"", "Badalona"", "Balenyà-Els Hostalets"", "Balenyà-Tona-Seva",
-			"Barberà del Vallès"", "Barcelona-Arc de Triomf"", "Barcelona-El Clot-Aragó"", "Barcelona-Estació de França",
-			"Barcelona-La Sagrera-Meridiana"", "Barcelona-Passeig de Gracia"", "Barcelona-Plaça de Catalunya",
-			"Barcelona-Sant Andreu Arenal"", "Barcelona-Sant Andreu Comtal"", "Barcelona-Sants"", "Barcelona-Torre del Baró",
-			"Bellvitge"", "Blanes"", "Borgonyà"", "Cabrera de Mar-Vilassar de Mar"", "Calafell"", "Caldes d'Estrac"", "Calella",
-			"Campdevànol"", "Canet de Mar"", "Cardedeu"", "Castellbell i el Vilar-Monistrol de Mont",
-			"Castellbisbal"", "Castelldefels"", "Centelles"", "Cerdanyola del Valĺès"", "Cerdanyola-Universitat",
-			"Cornellà"", "Cubelles"", "Cunit"", "El Masnou"", "El Papiol"", "El Prat de Llobregat"", "El Vendrell",
-			"Els Monjos"", "Figaro"", "Garraf"", "Gavà"", "Gelida"", "Granollers Centre"", "Granollers-Canovelles",
-			"Gualba"", "Hostalric"", "L'Arboç"", "L'Hospitalet de Llobregat"", "La Farga de Bebié"", "La Garriga",
-			"La Granada"", "La Llagosta"", "La Molina"", "La Tor de Querol-Enveig"", "Lavern-Subirats"", "Les Franqueses del Vallès",
-			"Les Franqueses-Granollers Nord"", "Llinars del VallÌs"", "Maçanet-Massanes"", "Malgrat de Mar"", "Manlleu"", "Manresa",
-			"Martorell"", "Mataró"", "Molins de Rei"", "Mollet-Sant Fost"", "Mollet-Santa Rosa"", "Montcada i Reixac",
-			"Montcada i Reixac-Manresa"", "Montcada i Reixac-Santa Maria"", "Montcada Bifurcació"", "Montcada Ripollet",
-			"Montgat"", "Montgat Nord"", "Montmeló"", "Ocata"", "Palautordera"", "Parets del Vallès"", "Pineda de Mar"", "Planoles",
-			"Platja de Castelldefels"", "Premià de Mar"", "Puigcerdà"", "Ribes de Freser"", "Riells i Viabrea-Breda"", "Ripoll",
-			"Rubí"", "Sabadell Centre"", "Sabadell Nord"", "Sabadell Sud"", "Sant Adrià del Besòs"", "Sant Andreu de Llavaneres",
-			"Sant Celoni"", "Sant Cugat del Vallés"", "Sant Feliu de Llobregat"", "Sant Joan Despí"", "Sant Martí de Centelles",
-			"Sant Miquel de Gonteres-Viladecavalls"", "Sant Pol de Mar"", "Sant Quirze de Besora"", "Sant Sadurní d'Anoia",
-			"Sant Vicenç de Calders"", "Sant Vicenç de Castellet"", "Santa Perpetua de Mogoda"", "Santa Susanna"", "Segur de Calafell",
-			"Sitges"", "Terrassa"", "Terrassa Est"", "Tordera"", "Torelló"", "Toses"", "Urtx-Alp"", "Vacarisses"", "Vacarisses-Torreblanca",
-			"Vic"", "Viladecans"", "Viladecavalls"", "Vilafranca del Penedés"", "Vilanova i la Geltrú"", "Vilassar de Mar"
-	};
-
-	private static final int[] STATION_IDS = new int[]{72400, 79600, 79404, 77106, 77107, 78705, 78804,
-			79009, 79400, 78806, 71802, 78805, 78802, 79004, 71801, 78801, 71708, 79606,
-			77112, 79412, 71601, 79502, 79603, 77301, 79601, 79101, 78605, 72210, 71705,
-			77105, 78706, 72503, 72303, 71604, 71603, 79407, 72211, 71707, 72201, 72203,
-			77103, 71703, 71706, 72208, 79100, 77006, 79105, 79107, 72202, 72305, 77114,
-			77102, 72205, 79011, 77306, 77310, 72206, 77100, 79109, 79102, 79200, 79605,
-			77110, 78600, 72209, 79500, 72300, 79006, 77004, 79005, 78708, 78707, 78800,
-			77002, 79405, 79406, 79007, 79408, 79103, 77005, 79604, 77304, 71704, 79409,
-			77309, 77303, 79106, 77200, 72501, 78704, 78709, 78703, 79403, 79501, 79104,
-			72502, 72301, 72302, 77104, 78610, 79602, 77113, 72207, 71600, 78604, 77003,
-			79608, 71602, 71701, 78700, 78710, 79607, 77111, 77305, 77307, 78606, 78607,
-			77109, 71709, 78609, 72204, 71700, 79410
-	};*/
-
-	/*public static final String[] STATION_NAMES = new String[]{
-			"Aeroport"", "Aguilar de Segarra"", "Alcover"", "Altafulla-Tamarit"", "Anglesola",
-			"Arenys de Mar"", "Ascó"", "Badalona"", "Balenyà-Els Hostalets"", "Balenyà-Tona-Seva",
-			"Barberà del Vallès"", "Barcelona-Arc de Triomf"", "Barcelona-El Clot-Aragó",
-			"Barcelona-Estació de França"", "Barcelona-La Sagrera-Meridiana"", "Barcelona-Passeig de Gràcia",
-			"Barcelona-Plaça de Catalunya"", "Barcelona-Sant Andreu Arenal"", "Barcelona-Sant Andreu Comtal",
-			"Barcelona-Sants"", "Barcelona-Torre del Baró"", "Bell-lloc d'Urgell"", "Bellpuig"", "Bellvitge",
-			"Blanes"", "Bordils-Juià"", "Borgonyà"", "Cabrera de Mar-Vilassar de Mar"", "Calaf"", "Calafell",
-			"Caldes d'Estrac"", "Caldes de Malavella"", "Calella"", "Camallera"", "Camarles-Deltebre"", "Cambrils",
-			"Camp-redó"", "Campdevànol"", "Canet de Mar"", "Capçanes"", "Cardedeu"", "Castellbell i el Vilar-Monistrol de Mont",
-			"Castellbisbal"", "Castelldefels"", "Castellnou de Seana"", "Celrà"", "Centelles"", "Cerbère",
-			"Cerdanyola del Vallès"", "Cerdanyola-Universitat"", "Cervera"", "Colera"", "Cornellà"", "Cubelles",
-			"Cunit"", "Duesaigües-L'Argentera"", "El Masnou"", "El Papiol"", "El Prat de Llobregat"", "El Vendrell",
-			"Els Guiamets"", "Els Monjos"", "Figaró"", "Figueres"", "Flaçà"", "Flix"", "Fornells de la Selva",
-			"Garraf"", "Gavà"", "Gelida"", "Girona"", "Golmés"", "Granollers Centre"", "Granollers-Canovelles",
-			"Gualba"", "Hostalric"", "Juneda"", "L'Aldea-Amposta"", "L'Ametlla de Mar"", "L'Ampolla-Perelló-Deltebre",
-			"L'Arboç"", "L'Espluga de Francolí"", "L'Hospitalet de Llobregat"", "L'Hospitalet de l'Infant",
-			"La Farga de Bebié"", "La Floresta"", "La Garriga"", "La Granada"", "La Llagosta"", "La Molina",
-			"La Plana-Picamoixons"", "La Riba"", "La Selva del Camp"", "La Tor de Querol-Enveig",
-			"Lavern-Subirats"", "Les Borges Blanques"", "Les Borges del Camp"", "Les Franqueses del Vallès",
-			"Les Franqueses-Granollers Nord"", "Llançà"", "Lleida-Pirineus"", "Llinars del Vallès",
-			"Malgrat de Mar"", "Manlleu"", "Manresa"", "Martorell"", "Marçà-Falset"", "Mataró",
-			"Maçanet-Massanes"", "Molins de Rei"", "Mollerussa"", "Mollet-Sant Fost",
-			"Mollet-Santa Rosa"", "Mont-roig del Camp"", "Montblanc"", "Montcada Bifurcació",
-			"Montcada Ripollet"", "Montcada i Reixac"", "Montcada i Reixac-Manresa"", "Montcada i Reixac-Santa Maria",
-			"Montgat"", "Montgat Nord"", "Montmeló"", "Móra la Nova"", "Nulles-Bràfim"", "Ocata"", "Palautordera",
-			"Parets del Vallès"", "Pineda de Mar"", "Planoles"", "Platja de Castelldefels"", "Port Aventura",
-			"Portbou"", "Pradell"", "Premià de Mar"", "Puigcerdà"", "Puigverd de Lleida-Artesa de Lleida",
-			"Rajadell"", "Reus"", "Riba-roja d'Ebre"", "Ribes de Freser"", "Riells i Viabrea-Breda"", "Ripoll",
-			"Riudecanyes-Botarell"", "Riudellots"", "Roda de Barà"", "Roda de Mar"", "Rubí"", "Sabadell Centre",
-			"Sabadell Nord"", "Sabadell Sud"", "Salomó"", "Salou"", "Sant Adrià del Besòs"", "Sant Andreu de Llavaneres",
-			"Sant Celoni"", "Sant Cugat del Vallès"", "Sant Feliu de Llobregat"", "Sant Guim de Freixenet",
-			"Sant Joan Despí"", "Sant Jordi Desvalls"", "Sant Martí Sesgueioles"", "Sant Martí de Centelles",
-			"Sant Miquel de Fluvià"", "Sant Miquel de Gonteres-Viladecavalls"", "Sant Pol de Mar",
-			"Sant Quirze de Besora"", "Sant Sadurní d'Anoia"", "Sant Vicenç de Calders"", "Sant Vicenç de Castellet",
-			"Santa Perpètua de Mogoda"", "Santa Susanna"", "Seguers-Sant Pere Sallavinera",
-			"Segur de Calafell"", "Sils"", "Sitges"", "Tarragona"", "Terrassa"", "Terrassa Est",
-			"Tordera"", "Torelló"", "Torredembarra"", "Tortosa"", "Toses"", "Tàrrega"", "Ulldecona-Alcanar-La Sénia",
-			"Urtx-Alp"", "Vacarisses"", "Vacarisses-Torreblanca"", "Valls"", "Vic"", "Vila-seca"", "Vilabella",
-			"Viladecans"", "Viladecavalls"", "Vilafranca del Penedès"", "Vilajuïga"", "Vilamalla",
-			"Vilanova i la Geltrú"", "Vilassar de Mar"", "Vilaverd"", "Vimbodí"", "Vinaixa"
-	};
-
-	private static final int[] STATION_IDS = new int[] {
-			72400, 78505, 73101, 71502, 78407, 79600, 71211, 79404, 77106, 77107, 78705,
-			78804, 79009, 79400, 78806, 71802, 78805, 78802, 79004, 71801, 78801, 78402,
-			78406, 71708, 79606, 79302, 77112, 79412, 78503, 71601, 79502, 79203, 79603,
-			79305, 65403, 65409, 65401, 77301, 79601, 71302, 79101, 78605, 72210, 71705,
-			78405, 79301, 77105, 79316, 78706, 72503, 78500, 79314, 72303, 71604, 71603,
-			71305, 79407, 72211, 71707, 72201, 71301, 72203, 77103, 79309, 79303, 71210,
-			79205, 71703, 71706, 72208, 79300, 78404, 79100, 77006, 79105, 79107, 73002,
-			65402, 65405, 65404, 72202, 73007, 72305, 65407, 77114, 73004, 77102, 72205,
-			79011, 77306, 73100, 73010, 73102, 77310, 72206, 73003, 71307, 77100, 79109,
-			79312, 78400, 79102, 79605, 77110, 78600, 72209, 71303, 79500, 79200, 72300,
-			78403, 79006, 77004, 65408, 73008, 78800, 77002, 79005, 78708, 78707, 79405,
-			79406, 79007, 71300, 76003, 79408, 79103, 77005, 79604, 77304, 71704, 65411,
-			79315, 71304, 79409, 77309, 73001, 78506, 71400, 71209, 77303, 79106, 77200,
-			71306, 79204, 72100, 72101, 72501, 78704, 78709, 78703, 76001, 65410, 79403,
-			79501, 79104, 72502, 72301, 78501, 72302, 79304, 78502, 77104, 79306, 78610,
-			79602, 77113, 72207, 71600, 78604, 77003, 79608, 78504, 71602, 79202, 71701,
-			71500, 78700, 78710, 79607, 77111, 71503, 65400, 77305, 78408, 65314, 77307,
-			78606, 78607, 76004, 77109, 71401, 76002, 71709, 78609, 72204, 79311, 79308,
-			71700, 79410, 73009, 73006, 73005
-	};*/
 
 	private static final LinkedHashMap<String, String> nucli50 = new LinkedHashMap<String, String>(){{
 		put("72400", "Aeroport");
@@ -503,6 +401,7 @@ public final class StationUtils {
 		put("10007", "Majadahonda");
 		put("70104", "Meco");
 		put("18003", "Mendez Alvaro");
+		put("97200", "Mirasierra - Paco de Lucia");
 		put("35606", "Mostoles");
 		put("35607", "Mostoles el Soto");
 		put("18002", "Nuevos Ministerios");
@@ -887,6 +786,8 @@ public final class StationUtils {
 		C510("#F9B410", Color.WHITE),
 		C710("#D70020", Color.WHITE),
 		C810("#088C29", Color.WHITE),
+		C8A10("#7D3494", Color.WHITE),
+		C8B10("#179447", Color.WHITE),
 		C910("#EE5513", Color.WHITE),
 		C1010("#93BC0E", Color.WHITE),
 		//Malaga
